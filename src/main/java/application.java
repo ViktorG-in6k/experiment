7462c373e45fc40f6c699b58b3dd4c8e3d3b2759
viktorg <viktorg@softwareplanet.uk.com>
2016-08-10 00:14:41 +0300

    add main app

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,5 +1,10 @@
-/**
- * Created by viktor on 09.08.16.
- */
-public class model {
+import model.Planet;
+import model.Planets;
+import model.SunSystem;
+
+public class application {
+    public static void main(String[] args) {
+        Planet planet = SunSystem.getPlanetByName(Planets.EARTH);
+        System.out.print(planet);
+    }
 }
