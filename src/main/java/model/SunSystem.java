0b2ce1d4f55fb87a9fafdbbd8fe5a538e83c5ebe
viktorg <viktorg@softwareplanet.uk.com>
2016-08-10 00:14:11 +0300

    add correct swith

--- src/main/java/model/SunSystem.java
+++ src/main/java/model/SunSystem.java
@@ -1,20 +1,40 @@
 package model;
 
 public class SunSystem {
-    ICore iCore = new Core();
-    public static final Planet Mercury = new Planet();
-    enum planets {
-        MERCURY,
-        VENERA,
-        EARTH,
-        MARS,
-        JUPITER,
-        SATURN,
-        URAN,
-        NEPTUN,
-        PLUTO
+
+    public static Planet getPlanetByName(Planets planetName) {
+        Planet planet = new Planet(0, 0);
+
+        switch (planetName) {
+            case MERCURY:
+                planet.setMassAndRadius(0.32868 * Math.pow(10, 24), 2.439 * Math.pow(10, 6));
+                break;
+            case VENEER:
+                planet.setMassAndRadius(4.81068 * Math.pow(10, 24), 6.052 * Math.pow(10, 6));
+                break;
+            case EARTH:
+                planet.setMassAndRadius(5.9760 * Math.pow(10, 24), 6.378 * Math.pow(10, 6));
+                break;
+            case MARS:
+                planet.setMassAndRadius(0.63345 * Math.pow(10, 24), 3.488 * Math.pow(10, 6));
+                break;
+            case JUPITER:
+                planet.setMassAndRadius(1876.64328 * Math.pow(10, 24), 71.300 * Math.pow(10, 6));
+                break;
+            case SATURN:
+                planet.setMassAndRadius(561.80376 * Math.pow(10, 24), 60.100 * Math.pow(10, 6));
+                break;
+            case URAN:
+                planet.setMassAndRadius(86.05440 * Math.pow(10, 24), 26.500 * Math.pow(10, 6));
+                break;
+            case NEPTUNE:
+                planet.setMassAndRadius(101.59200 * Math.pow(10, 24), 24.750 * Math.pow(10, 6));
+                break;
+            case PLUTO:
+                planet.setMassAndRadius(0.01195 * Math.pow(10, 24), 2.000 * Math.pow(10, 6));
+                break;
+        }
+
+        return planet;
     }
-
-
-
 }
