3ab7204c78bdc26f4cc1c3e54615fcb5b15b09fa
viktorg <viktorg@softwareplanet.uk.com>
2016-08-10 00:15:13 +0300

    add test for planet

--- src/test/java/model/PlanetTest.java
+++ src/test/java/model/PlanetTest.java
@@ -1,7 +1,17 @@
 package model;
 
-/**
- * Created by viktor on 09.08.16.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlanetTest {
+    @Test
+    public void planetObjectShouldReturnParametersOfPlanetEarth() throws Exception {
+        Planet planet = SunSystem.getPlanetByName(Planets.EARTH);
+
+        assertEquals(planet.getMass(), 5.975999999999999E24, 10);
+        assertEquals(planet.getRadius(), 6378000.0, 10);
+        assertEquals(planet.getDensity(), 5498.80646732255, 10);
+        assertEquals(planet.getVolume(), 1.0867812925428892E21, 10);
+    }
 }
